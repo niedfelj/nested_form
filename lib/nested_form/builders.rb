00c2e40176edd6c2c36dc9bcfb824dc9207806d4
Joel Niedfeldt <joel@niedfeldt.com>
2012-05-10 18:07:57 -0400

    support for config of formbuilder

--- lib/nested_form/builders.rb
+++ lib/nested_form/builders.rb
@@ -15,7 +15,7 @@ module NestedForm
 
   begin
     require 'formtastic'
-    class FormtasticBuilder < (defined?(::Formtastic::FormBuilder) ? Formtastic::FormBuilder : ::Formtastic::SemanticFormBuilder)
+    class FormtasticBuilder < Formtastic::Helpers::FormHelper.builder
       include ::NestedForm::BuilderMixin
     end
   rescue LoadError
